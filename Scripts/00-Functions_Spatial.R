81d2a818113fc1fea5f5d23f44f82d2819140c9f
Pablo <pmbusch@uc.cl>
2022-03-01 12:28:54 -0800

    Re-factor code to split code for spatial layers functions

---
+++ Scripts/00-Functions_Spatial.R
@@ -0,0 +1,117 @@
+# Functions to create interactive maps using Leaflet
+# 
+# PBH March 2022
+
+
+## Spatial Interactive Visualization function ----
+# Create spatial layer map with labels, to quickly visualize it
+# Inputs: 
+# - sf object (spatial vector)
+# - label vector: Label to display on the map
+# - polygon form: "a" for polygon area (default), "p" for point and "l" for line
+f.interactive.map <- function(map_object, label_vector, polygon_form="a",...){
+  
+  # Create labels for the vectors
+  labels <- sprintf(
+    "<strong>%s</strong><br/>",
+    label_vector) %>% 
+    lapply(HTML)
+  
+  polygon_form <- str_to_lower(polygon_form)
+  
+  # create interactive map object
+  if (polygon_form=="a"){
+    map_interactive <- leaflet(map_object) %>% 
+      addTiles() %>% 
+      addPolygons(label = labels,...)
+  } else if (polygon_form=="p") {
+    map_interactive <- leaflet(map_object) %>% 
+      addTiles() %>% 
+      addCircles(label = labels,...)
+  } else {
+    map_interactive <- leaflet(map_object) %>% 
+      addTiles() %>% 
+      addPolylines(label = labels,...)
+  }
+  # return map
+  return(map_interactive)
+}
+
+
+# Function to create popup for the spatial layers ------
+# Inputs:
+# - Layer: sf object, to get the feature
+# - Title of the layer
+# - Features: Vector of character of features to print
+f.create.labels <- function(layer,title, features){
+  
+  # Extract features to print from the layer
+  features_data <- layer %>% as.data.frame() %>% dplyr::select(features)
+  
+  # Incorporate column name into the feature to print
+  feat <- sapply(colnames(features_data), 
+                 function(x) sprintf("<i>%s</i>: %s",
+                                     x,features_data[,x])) %>%
+    as.data.frame()
+  # Merge all columns into a single text, separated by a line break
+  feat2 <- unite(feat,"x",colnames(feat),sep="<br/>")
+  
+  # Add title
+  feat2$x <- paste0("<strong>",title,"</strong><br/>",feat2$x)
+  
+  # Convert to HTML to incorporate into the map
+  label <- feat2$x %>% as.list() %>% lapply(HTML)
+  
+  # return
+  return(label)
+}
+
+## Function to add layers with a specific format
+# - layer: sf object (spatial vector)
+# - Title: Title to display on the map
+# - polygon form: "a" for polygon area (default), "p" for point and "l" for line
+
+
+label_options <- labelOptions(
+  style = list("font-weight" = "normal", padding = "3px 8px"),
+  textsize = "12px",
+  direction = "auto")
+
+add.Layer <- function(map,layer,
+                      title,features,
+                      polygon_form="a",
+                      group_l="default",
+                      ...){
+  
+  group_l <- if (group_l=="default") title else group_l
+  polygon_form <- str_to_lower(polygon_form)
+  label_out <- f.create.labels(layer,title,features)
+  
+  if (polygon_form=="a"){
+    addPolygons(map,data=layer,
+                label=label_out,
+                group=group_l,
+                labelOptions = label_options,
+                ...)
+  } else if(polygon_form=="p"){
+    addCircles(map,data=layer,
+               label=label_out,
+               group=group_l,
+               labelOptions = label_options,
+               ...)
+  } else {
+    addPolylines(map,data=layer,
+                 label=label_out,
+                 group=group_l,
+                 labelOptions = label_options,
+                 ...)
+  }
+  
+}
+
+
+
+
+
+
+# EoF
--- Scripts/Spatial_Layers.R
+++ Scripts/Spatial_Layers.R
@@ -5,70 +5,8 @@
 
 # Load libraries and other useful common functions -----
 source("Scripts/00-Common.R", encoding = "UTF-8")
-
+source("Scripts/00-Functions_Spatial.R",encoding = "UTF-8")
 url_load_shp <- "Data/Spatial Data/%s.rds"
-
-## Spatial Interactive Visualization function ----
-# Create spatial layer map with labels, to quickly visualize it
-# Inputs: 
-# - sf object (spatial vector)
-# - label vector: Label to display on the map
-# - polygon form: "a" for polygon area (default), "p" for point and "l" for line
-f.interactive.map <- function(map_object, label_vector, polygon_form="a",...){
-  
-  # Create labels for the vectors
-  labels <- sprintf(
-    "<strong>%s</strong><br/>",
-    label_vector) %>% 
-    lapply(HTML)
-  
-  polygon_form <- str_to_lower(polygon_form)
-  
-  # create interactive map object
-  if (polygon_form=="a"){
-    map_interactive <- leaflet(map_object) %>% 
-      addTiles() %>% 
-      addPolygons(label = labels,...)
-  } else if (polygon_form=="p") {
-    map_interactive <- leaflet(map_object) %>% 
-      addTiles() %>% 
-      addCircles(label = labels,...)
-  } else {
-    map_interactive <- leaflet(map_object) %>% 
-      addTiles() %>% 
-      addPolylines(label = labels,...)
-  }
-  # return map
-  return(map_interactive)
-}
-
-# Function to create popup for the spatial layers ------
-# Inputs:
-# - Layer: sf object, to get the feature
-# - Title of the layer
-# - Features: Vector of character of features to print
-f.create.labels <- function(layer,title, features){
-  
-  # Extract features to print from the layer
-  features_data <- layer %>% as.data.frame() %>% dplyr::select(features)
-  
-  # Incorporate column name into the feature to print
-  feat <- sapply(colnames(features_data), 
-                 function(x) sprintf("<i>%s</i>: %s",
-                                     x,features_data[,x])) %>%
-    as.data.frame()
-  # Merge all columns into a single text, separated by a line break
-  feat2 <- unite(feat,"x",colnames(feat),sep="<br/>")
-  
-  # Add title
-  feat2$x <- paste0("<strong>",title,"</strong><br/>",feat2$x)
-  
-  # Convert to HTML to incorporate into the map
-  label <- feat2$x %>% as.list() %>% lapply(HTML)
-  
-  # return
-  return(label)
-}
 
 
 ## Communes  and labels ------
@@ -135,49 +73,6 @@ comunidad_indigena <- readRDS(sprintf(url_load_shp,"comunidad_indigena"))
 ## FEATURE DATA -------
 
 ## Map with all layers ------
-
-## Function to add layers with a specific format
-# - layer: sf object (spatial vector)
-# - Title: Title to display on the map
-# - polygon form: "a" for polygon area (default), "p" for point and "l" for line
-add.Layer <- function(map,layer,
-                      title,features,
-                      polygon_form="a",
-                      group_l="default",
-                      ...){
-  
-  group_l <- if (group_l=="default") title else group_l
-  polygon_form <- str_to_lower(polygon_form)
-  label_out <- f.create.labels(layer,title,features)
-  label_options <- labelOptions(
-    style = list("font-weight" = "normal", padding = "3px 8px"),
-    textsize = "12px",
-    direction = "auto")
-  
-  if (polygon_form=="a"){
-    addPolygons(map,data=layer,
-               label=label_out,
-               group=group_l,
-               labelOptions = label_options,
-               ...)
-  } else if(polygon_form=="p"){
-  addCircles(map,data=layer,
-             label=label_out,
-             group=group_l,
-             labelOptions = label_options,
-             ...)
-  } else {
-    addPolylines(map,data=layer,
-               label=label_out,
-               group=group_l,
-               labelOptions = label_options,
-               ...)
-  }
-  
-}
-
-
-
 m <- leaflet() %>% 
   addTiles() %>% 
   add.Layer(lagos_zone,lagos_zone$Nombre,
